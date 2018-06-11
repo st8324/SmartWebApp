1587a3bd07a0d1ec362d16848312249ad841966c
Unknown <stajun@naver.com>
2018-06-11 17:21:50 +0900

    javascript 5회차 : 회원가입 페이지 유효성 검사

--- javascript/day5/js/join.js
+++ javascript/day5/js/join.js
@@ -5,20 +5,35 @@ formTag.onsubmit = function(){
     var id = document.getElementById('usr').value;
     var pwd = document.getElementById('pwd').value;
     var pwdConfirm = document.getElementById('pwdConfirm').value;
+    var year = document.getElementById('year').value;
+    var month = document.getElementById('month').value;
+    var day = parseInt(document.getElementById('day').value);
+    var max = maxDay(month);
+    var number = document.getElementById('number').value;
+    var numberRegex =/^010\d{8}$/;
+
     var inforId = document.getElementById('inforId');
     var inforPwd = document.getElementById('inforPwd');
     var inforPwdConfirm = document.getElementById('inforPwdConfirm');
+    var inforBirth = document.getElementById('inforBirth');
+    var inforNumber = document.getElementById('inforNumber');
 
     inforId.style.display = 'none';
     inforPwd.style.display = 'none';
     inforPwdConfirm.style.display = 'none';
+    inforBirth.style.display = 'none';
+    inforNumber.style.display = 'none';
+
     var isOk = true;
+    var idRegex = /^[a-zA-Z]\w{4,9}$/;
+    var pwdRegex = /^(?=\w{8,20}$)\w*(\d[A-z]|[A-z]\d)\w*$/;
+
     //id의 길이가 0이면
-    if(id.length == 0){
+    if(id.length == 0 || !idRegex.test(id)){
         inforId.style.display = 'block';
         isOk = false;
     }
-    if(pwd.length == 0){
+    if(pwd.length == 0 || !pwdRegex.test(pwd)){
         inforPwd.style.display = 'block';
         isOk = false;
     }
@@ -26,6 +41,35 @@ formTag.onsubmit = function(){
         inforPwdConfirm.style.display = 'block';
         isOk = false;
     }
+    
+    if(year.length != 4){
+        inforBirth.innerHTML = '태어난 년도 4자리를 정확하게 입력하세요.';
+        inforBirth.style.display = 'block';
+        isOk = false;
+    }else if(isNaN(parseInt(month))){
+        inforBirth.innerHTML = '태어난 월을 선택하세요.';
+        inforBirth.style.display = 'block';
+        isOk = false;
+    }else if(isNaN(day) || day < 1 || day > max){
+        inforBirth.innerHTML = '태어난 일(날짜) 2자리를 정확하게 입력하세요.';
+        inforBirth.style.display = 'block';
+        isOk = false;
+    }
+    if(!numberRegex.test(number)){
+        inforNumber.style.display = 'block';
+        isOk = false;
+    }
+    
     if(!isOk)
         return false;
+}
+function maxDay(month){
+    switch(month){
+        case '1': case '3': case '5': case '7': case '8': case '10': case '12':
+            return 31;
+        case '2':
+            return 28;
+        default:
+            return 30;
+    }
 }
