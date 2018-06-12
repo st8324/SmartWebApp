e73e7dbaf7b702dce3f2b8cb5a24c230cfa5b96d
Unknown <stajun@naver.com>
2018-06-12 15:38:15 +0900

    javascript 6회차 : stopImmediatePropagation 예제

---
+++ javascript/day6/js/stopImmediatePropagationEx.js
@@ -0,0 +1,37 @@
+function fnload(){
+    var eventPhase = {
+        0:'NONE',
+        1:'CAPTURE',
+        2:'TARGET',
+        3:'BUBBLE'
+    };
+    var depth1 = document.getElementById('depth1');
+    var depth2 = document.getElementById('depth2');
+    var depth3 = document.getElementById('depth3');
+    var depth4 = document.getElementById('depth4');
+
+    depth1.addEventListener('click',function(e){
+        console.log(eventPhase[e.eventPhase] +' : depth1' );
+    },false);
+    depth2.addEventListener('click',function(e){
+        console.log(eventPhase[e.eventPhase] +' : depth2' );
+    },false);
+
+    depth3.addEventListener('click',function(e){
+        console.log(eventPhase[e.eventPhase] +' : depth3 : first' );
+        e.stopPropagation();
+    },false);
+    depth3.addEventListener('click',function(e){
+        console.log(eventPhase[e.eventPhase] +' : depth3 : second' );
+        e.stopPropagation();
+    },false);
+
+    depth4.addEventListener('click',function(e){
+        console.log(eventPhase[e.eventPhase] +' : depth4 : first' );
+        e.stopImmediatePropagation();
+    },false);
+    depth4.addEventListener('click',function(e){
+        console.log(eventPhase[e.eventPhase] +' : depth4 : second' );
+        e.stopImmediatePropagation();
+    },false);
+}
