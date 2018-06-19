b06bf94b5a1c781258f2346cf9ea081828803986
Unknown <stajun@naver.com>
2018-06-19 15:50:12 +0900

    포트폴리오-프론트 : 햄버거 메뉴 수정

--- portfolio/front-end/js/page1.js
+++ portfolio/front-end/js/page1.js
@@ -1,11 +1,15 @@
 $("#hamburger").click(function() {
 	$("#sidenav").css("right", "0");
-  $("body").addClass("dark")
+  // $("body").addClass("dark");
+  $("body").css("background","rgba(0,0,0,0.4)");
+  $("img").css("opacity","0.5");
 });
 $("#close-sidenav").click(function() {
 	$("#sidenav").css("right", "-260px");
   $("body").removeClass("dark");
+  $("img").css("opacity","1");
+  $("body").css("background","none");
 });
 $('.carousel').carousel({
-  interval: 2000
+  interval: 4000
 });
