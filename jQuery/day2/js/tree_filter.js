e1441ee49060911b22a54a584982905852b6d74d
Unknown <stajun@naver.com>
2018-07-03 10:16:02 +0900

    jQuery 2회차 : 검색 필터 예제

---
+++ jQuery/day2/js/tree_filter.js
@@ -0,0 +1,16 @@
+// 부모의 id가 filter이고 자식이 div인 요소를 선택
+
+
+$(".filter>div").eq(2).addClass("yellow");
+$(".filter>div").first().css("background","green");
+$(".filter>div").last().css("background","gray");
+
+$(".filter>div").slice(3,5).css("font-weight","bold");
+$(".filter>div").not(":eq(2)").css("border","dotted");
+
+$(".filter>div").bind("click",function(){
+    if($(this).is(".filter>div:eq(2)")){
+        //선택된 요소의 글자색을 빨간색으로 수정
+        $(this).css("color","red");
+    }
+});
