1c56b94eef4287c9af68217529bd8f854486928c
Unknown <stajun@naver.com>
2018-07-24 17:07:45 +0900

    네이버 예제 3일차 날씨부분 기능구현

--- Example/js/naver.js
+++ Example/js/naver.js
@@ -17,4 +17,40 @@ $(document).ready(function(){
         $('#btns').toggleClass('display-none');
         $('.realtime-search').toggleClass('display-none');
     });
+    $('#close').click(function(){
+        $('#menu-contents').toggleClass('display-none');
+        $('.container').toggleClass('dark');
+        $('.more-up').toggleClass('display-none');
+        $('.more-down').toggleClass('display-none');
+        $('#btns').toggleClass('display-none');
+        $('.realtime-search').toggleClass('display-none');
+    });
+
+    var index = 0;
+
+    $('#next').click(function(){
+        
+        index = (index+1)%6;
+        
+        $('#etcContents div').eq(index).removeClass('display-none');    
+        $('#etcContents div').eq(index).addClass('display-block');
+        
+        $('#etcContents div').not($('#etcContents div').eq(index))
+            .removeClass('display-block');
+        $('#etcContents div').not($('#etcContents div').eq(index))
+            .addClass('display-none');
+    });
+    $('#prev').click(function(){
+        
+        index = (index-1)%6;
+        if(index == -1)
+            index = 5;
+        $('#etcContents div').eq(index).removeClass('display-none');    
+        $('#etcContents div').eq(index).addClass('display-block');
+        
+        $('#etcContents div').not($('#etcContents div').eq(index))
+            .removeClass('display-block');
+        $('#etcContents div').not($('#etcContents div').eq(index))
+            .addClass('display-none');
+    });
 });
